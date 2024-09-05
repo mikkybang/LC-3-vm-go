fae8ffc36e182eddefc9a197523203e66e4abd72
Michael Efemena <mefemena@gmail.com>
2024-09-05 21:19:31 +0100

    feat: add registers const and init to iota

--- main.go
+++ main.go
@@ -3,7 +3,26 @@ package main
 import "fmt"
 
 const MEMORY_MAX = (1 << 16)
+
 var memory [MEMORY_MAX]uint16
+
+const (
+	R_R0 = iota
+	R_R1
+	R_R2
+	R_R3
+	R_R4
+	R_R5
+	R_R6
+	R_R7
+	R_PC /* program counter */
+	R_COND
+	R_COUNT
+)
+
+var registers [R_COUNT]uint16
+
 func main() {
 	fmt.Println("Hello From VM")
+	fmt.Print(memory)
 }
