ebba68faa3f30f35f5c540816c7fa337b916daf0
Michael Efemena <mefemena@gmail.com>
2024-10-01 14:50:18 +0100

    feat: add Oprand enums and conditions flags

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ const MEMORY_MAX = (1 << 16)
 
 var memory [MEMORY_MAX]uint16
 
+// Register Enums
 const (
 	R_R0 = iota
 	R_R1
@@ -22,7 +23,48 @@ const (
 
 var registers [R_COUNT]uint16
 
+// Operand Enums
+const (
+	OP_BR   = iota /* branch */
+	OP_ADD         /* add  */
+	OP_LD          /* load */
+	OP_ST          /* store */
+	OP_JSR         /* jump register */
+	OP_AND         /* bitwise and */
+	OP_LDR         /* load register */
+	OP_STR         /* store register */
+	OP_RTI         /* unused */
+	OP_NOT         /* bitwise not */
+	OP_LDI         /* load indirect */
+	OP_STI         /* store indirect */
+	OP_JMP         /* jump */
+	OP_RES         /* reserved (unused) */
+	OP_LEA         /* load effective address */
+	OP_TRAP        /* execute trap */
+)
+
+// Condition flags
+const (
+	FL_POS = 1 << 0 // P
+	FL_ZRO = 1 << 1 // Z
+	FL_NEG = 1 << 2 // N
+)
+
+const (
+	PC_START = 0x3000
+)
+
 func main() {
 	fmt.Println("Hello From VM")
-	fmt.Print(memory)
+
+	// Set the condition flag to the Z flag
+	registers[R_COND] = FL_ZRO
+
+	// Set the program counter to starting position
+	registers[R_PC] = PC_START
+
+	isRunning := true
+
+	for isRunning {
+	}
 }
